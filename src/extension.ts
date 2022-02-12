e53809c542ae3bd37d217be7bb340687d451098f
tinaxd <31384595+tinaxd@users.noreply.github.com>
2022-02-13 01:54:12 +0900

    delete function

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,44 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { spawn } from 'child_process';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "wsl-safe-delete" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('wsl-safe-delete.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from SafeDeleteWSL!');
-	});
+	// // Use the console to output diagnostic information (console.log) and errors (console.error)
+	// // This line of code will only be executed once when your extension is activated
+	// console.log('Congratulations, your extension "wsl-safe-delete" is now active!');
 
 	const moveToTrash = vscode.commands.registerCommand('wsl-safe-delete.moveToTrash', (args) => {
-		console.log(args);
+		// console.log(args);
+		if (args === null) {
+			return;
+		}
+		const path = args.path as string;
+		const scheme = args.scheme as string;
+
+		if (scheme !== 'file') {
+			return;
+		}
+
+		if (path === null) {
+			return;
+		}
+
+		vscode.window.showInformationMessage(`Are you sure you want to delete ${path}?`, ...["Yes", "No"])
+		.then((answer) => {
+			if (answer === 'Yes') {
+				const trash = spawn('trash-put', [path]);
+				trash.on('close', (code) => {
+					console.log(`trash-put exited with code ${code}`);
+				});
+			}
+		});
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(moveToTrash);
 }
 
 // this method is called when your extension is deactivated
